06afba91530fb77cfc6433e9db4bf770176c84a5
starfoxdot64 <starfox.64@outlook.com>
2023-02-11 22:37:22 -0800

    prepare.sh: Implemented blacklisted key checks

--- tests/prepare.sh
+++ tests/prepare.sh
@@ -13,6 +13,18 @@ then
     exit 1
 fi
 
+# Check if xmlstarlet is available, and offer minor help if it is not
+if ! command -v xmlstarlet &> /dev/null
+then
+    echo ""
+    echo ""
+    echo "We couldn't find the xmlstarlet command on your system."
+    echo "Without that, we can't parse XML files. Install the"
+    echo "xmlstarlet package, then try again."
+    echo ""
+    echo ""
+    exit 1
+fi
 
 # Check for an input file
 if [ $# -ne 1 ]; then
@@ -45,35 +57,48 @@ fi
 # Read the lines in knownKeys into an array
 readarray -t known_keys < knownKeys
 
+# Read the blacklist into an array
+readarray -t blacklist < blacklist
+
 # Create a temporary file to store the updated XML
 temp_file=$(mktemp)
 
-# Write an XML header and a resources tag to the temporary file
+# Create a string to hold an XPath expression to search with,
+# then Loop over each line in the known_keys array (starting from the second element)
+xpath_expression_known="//*[@name='${known_keys[0]}'"
+for key in "${known_keys[@]:1}"; do
+  # Add the current key to the XPath expression
+  xpath_expression_known="$xpath_expression_known or @name='$key'"
+done
+xpath_expression_known="$xpath_expression_known]"
+
+# Write an XML header and a resources tag to the temporary file, since xmllint gets rid of it later
+# Reminder to self: > will clear the file, >> appends to it
 echo "<?xml version='1.0' encoding='utf-8'?>" > "$temp_file"
 echo "<resources>" >> "$temp_file"
 
-# Create a string to hold an XPath expression to search with
-xpath_expression="//*[@name='${known_keys[0]}'"
+# Select nodes that match our known keys and put them in the tempfile
+xmllint --xpath "$xpath_expression_known" "$input_file" >> "$temp_file"
 
-# Loop over each line in the known_keys array (starting from the second element)
-for key in "${known_keys[@]:1}"; do
-  # Add the current key to the XPath expression
-  xpath_expression="$xpath_expression or @name='$key'"
+# Create another xpath expression to carry blacklisted keys, doing the same as with the known keys
+xpath_expression_blacklist="//*[@name='${blacklist[0]}'"
+for key in "${blacklist[@]:1}"; do
+  xpath_expression_blacklist="$xpath_expression_blacklist or @name='$key'"
 done
+xpath_expression_blacklist="$xpath_expression_blacklist]"
 
-# Close the XPath expression
-xpath_expression="$xpath_expression]"
-
-# Use xmllint to select nodes that match our criteria
-xmllint --xpath "$xpath_expression" "$input_file" >> "$temp_file"
-
-# Add the closing resources tag to the temporary file
+# Add closing tag, because xmllint doesn't carry it over
 echo "</resources>" >> "$temp_file"
 
-# Replace the input XML file with the temporary file
-mv "$temp_file" "$input_file"
+# Use xmlstarlet to select nodes we don't want, remove them, and put them in a new tempfile
+xmlstarlet ed --delete "$xpath_expression_blacklist" "$temp_file" > "$temp_file-2"
+
+# Swap out the input file after final edits, then clean up the last tempfile
+mv "$temp_file-2" "$input_file"
+rm "$temp_file"
 
 # Inform user of success
+echo ""
 echo ""
 echo "Done editing $input_file."
 echo ""
@@ -81,8 +106,9 @@ echo "Caution: Please be aware that this script may make"
 echo "errors - Check the results before you build, and be"
 echo "sure to use tests.sh to ensure nothing was missed."
 echo ""
-echo "This script does not look for blacklisted keys, so"
-echo "you may still have a few keys in the XML that will"
-echo "require manual removals. It's better than dozens!"
+echo "This script should be able to find blacklisted keys"
+echo "and take care of them automatically, but there could"
+echo "still have a few keys in the XML that will require"
+echo "manual removals. It's better than dozens though!"
 echo ""
 echo ""
