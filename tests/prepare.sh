fa6cc98332f69f99af43bc3211feda94ae09550c
starfoxdot64 <starfox.64@outlook.com>
2023-02-11 19:48:53 -0800

    Updated prepare.sh to be a little more user-friendly

--- tests/prepare.sh
+++ tests/prepare.sh
@@ -1,23 +1,45 @@
 #!/bin/bash
 
-# Check if an input file was provided
+# Check if xmllint is available, and offer minor help if it is not
+if ! command -v xmllint &> /dev/null
+then
+    echo ""
+    echo ""
+    echo "We couldn't find the xmllint command on your system."
+    echo "Without that, we can't parse XML files. Install the"
+    echo "libxml2-utils package, then try again."
+    echo ""
+    echo ""
+    exit 1
+fi
+
+
+# Check for an input file
 if [ $# -ne 1 ]; then
   echo ""
   echo ""
   echo "Error: Input XML not provided."
   echo ""
-  echo "Usage: $0 inputFile.xml"
+  echo "Usage: $0 path/to/inputFile.xml"
   echo ""
-  echo "Keep in mind, this script will overwrite your input file. Make a backup!"
-  exit 1
+  echo "Keep in mind, this script is experimental, and will"
+  echo "overwrite your input file. If you haven't already,"
+  echo "make a backup!"
+  echo ""
+  echo ""
+  exit 2
 fi
 
 input_file="$1"
 
 # Check if the input file exists
 if [ ! -f "$input_file" ]; then
+  echo ""
+  echo ""
   echo "Error: Input XML $input_file not found. Double-check your filename."
-  exit 2
+  echo ""
+  echo ""
+  exit 3
 fi
 
 # Read the lines in knownKeys into an array
@@ -30,7 +52,7 @@ temp_file=$(mktemp)
 echo "<?xml version='1.0' encoding='utf-8'?>" > "$temp_file"
 echo "<resources>" >> "$temp_file"
 
-# Create a string to hold the XPath expression
+# Create a string to hold an XPath expression to search with
 xpath_expression="//*[@name='${known_keys[0]}'"
 
 # Loop over each line in the known_keys array (starting from the second element)
@@ -42,7 +64,7 @@ done
 # Close the XPath expression
 xpath_expression="$xpath_expression]"
 
-# Use xmllint to select nodes that match the XPath expression
+# Use xmllint to select nodes that match our criteria
 xmllint --xpath "$xpath_expression" "$input_file" >> "$temp_file"
 
 # Add the closing resources tag to the temporary file
